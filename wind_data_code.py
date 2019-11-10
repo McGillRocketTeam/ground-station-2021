86d8d01d0b7310b9a1236e0bac7deda8ce597cb6
Alex Stedman <stedman24@gmail.com>
2019-11-10 12:25:30 -0500

    Add data format file Update wind data code

--- wind_data_code.py
+++ wind_data_code.py
@@ -6,66 +6,86 @@ import os
 import sys
 
 #NOTE: for all parameters, a value of -9999 means that the value does not exist. 
+class Wind():
 
-#Use the datetime package to get time
-now = datetime.datetime.utcnow() #Get time right now in utc time
-hour_ago = datetime.datetime.utcnow()-datetime.timedelta(hours=5) #Get time one hour ago in utc time
-yesterday = datetime.datetime.utcnow()-datetime.timedelta(days=1) #Get time yesterday in utc time
+	def __init__(self):
+		#Use the datetime package to get time
+		now = datetime.datetime.utcnow() #Get time right now in utc time
+		hour_ago = datetime.datetime.utcnow()-datetime.timedelta(hours=5) #Get time one hour ago in utc time
+		yesterday = datetime.datetime.utcnow()-datetime.timedelta(days=1) #Get time yesterday in utc time
 
-#Set begining and end times 
-end_date = now.strftime("%Y %m %d")
-begin_date = yesterday.strftime("%Y %m %d")
-print(begin_date)
+		#Set begining and end times
+		end_date = now.strftime("%Y %m %d")
+		begin_date = yesterday.strftime("%Y %m %d")
+		print(begin_date)
 
-fl=open("USM00072364-data.txt","r")
+		fl = open("./USM00072364_data.txt", "r")
 
-fl = fl.readlines()
-date = begin_date+" 12"
-i = 0
-for line in fl:
-	i = i + 1
-	if date in line:
-		print (line) 
-		#print(i)
-		break
+		fl = fl.readlines()
+		date = begin_date+" 12"
+		i = 0
+		for line in fl:
+			i = i + 1
+			if date in line:
+				print(line)
+				#print(i)
+				break
 
-j = i
-print(j)
-num_lines = sum(1 for line in fl)
-print(num_lines)
+		j = i
+		print(j)
+		num_lines = sum(1 for line in fl)
+		print(num_lines)
 
-gph = [] #in in m above sea level
-wdir = [] #in deg from N
-wspd = [] #in m/s
-press = [] #in MPa
-time = []
+		self.gph = []  # in in m above sea level
+		self.wdir = []  # in deg from N
+		self.wspd = []  # in m/s
+		self.press = []  # in MPa
+		self.time = []
 
-while (j < num_lines):
-	x1 = fl[j].split()[3]
-	x2 = fl[j].split()[7]
-	x3 = fl[j].split()[8]
-	x4 = fl[j].split()[2]
-	x5 = fl[j].split()[1]
-	
-	if (x1[-1] == 'B'):
-		x1 = x1[:-1]
-		
-	gph.append(int(x1))
-	wdir.append(int(x2))
-	wspd.append(int(x3)/10)
-	time.append(int(x5))
-	if (x4[-1]=='B'):
-		x4 = x4[:-1]
-	press.append(int(x4)/1000)
-	
-		
+		for index, current_set in enumerate(fl[1:]):
 
-	j = j + 1
-	
-print(gph)
-#print(time)
-print(wdir)
-print(wspd)
-#print(press)
+			val_split = current_set.split()
+
+			if len(val_split) < 9:
+				continue
+
+			geo_potent_height = val_split[3]
+			wind_direction = val_split[7]
+			wind_speed = val_split[8]
+			air_pressure = val_split[2]
+			current_time = val_split[1]
+
+			if geo_potent_height[-1] == 'B':
+				geo_potent_height = geo_potent_height[:-1]
 
 
+			try:
+				t1 = int(geo_potent_height)
+				t2 = int(wind_direction)
+				t3 = int(wind_speed)
+			except:
+				continue
+			self.gph.append(int(geo_potent_height))  # this list is the altitude above sea level
+			self.wdir.append(int(wind_direction))  # this is wind direction
+			self.wspd.append(int(wind_speed)/10)	 # wind speed
+			self.time.append(int(current_time))
+			if air_pressure[-1] == 'B' or air_pressure[-1] == 'A':
+				air_pressure = air_pressure[:-1]
+			self.press.append(int(air_pressure)/1000)
+
+		print(len(self.gph))
+		#print(time)
+		print(len(self.wdir))
+		print(len(self.wspd))
+		#print(press)
+
+	def get_wind_data_tuple(self):
+		'''
+		:return: a tuple (height, wind_dir, wind_speed)
+		'''
+		return self.gph, self.wdir, self.wspd
+
+
+# wd = Wind()
+# x = wd.get_wind_data_tuple()
+# print(x)
