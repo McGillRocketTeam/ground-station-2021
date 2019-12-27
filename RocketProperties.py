1a33e4cd099e9f6c7349d4d07ab7bda1a7a3054b
Charles Renshaw Whitman <charles.whitman@mail.mcgill.ca>
2019-12-27 14:31:55 -0500

    Update RocketProperties.py

--- RocketProperties.py
+++ RocketProperties.py
@@ -2,7 +2,7 @@ class RocketProperties:
     # This class contains everything which should be constant for a given set of simulations
     # All numbers are currently incorrect.
     def __init__(self, main_deploy_altitude, apogee, velocity_off_rail_mag, vertical_drag_coeff_drogue,
-    vertical_drag_coeff_drogue, vertical_drag_coeff_main, transverse_drag_coeff_drogue, transverse_drag_coeff_main,
+    vertical_drag_coeff_main vertical_drag_coeff_main, transverse_drag_coeff_drogue, transverse_drag_coeff_main,
     drogue_deploys_bool, main_deploys_bool):
     # Numerical Values which shouldn't change between sets of simulations
         self.main_deploy_altitude = main_deploy_altitude # meters
