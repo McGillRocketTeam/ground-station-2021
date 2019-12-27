d8847d099b962aec8861a2ed0f8074c49071beb5
Charles Renshaw Whitman <charles.whitman@mail.mcgill.ca>
2019-12-27 13:26:36 -0500

    fixed RocketProperties to allow input

--- RocketProperties.py
+++ RocketProperties.py
@@ -1,14 +1,20 @@
 class RocketProperties:
     # This class contains everything which should be constant for a given set of simulations
     # All numbers are currently incorrect.
-    main_deploy_altitude = 1 # meters
-    apogee = 1 # meters
-    velocity_off_rail_mag = 1 # m/s
+    def __init__(self, main_deploy_altitude, apogee, velocity_off_rail_mag, vertical_drag_coeff_drogue,
+    vertical_drag_coeff_drogue, vertical_drag_coeff_main, transverse_drag_coeff_drogue, transverse_drag_coeff_main,
+    drogue_deploys_bool, main_deploys_bool):
+    # Numerical Values which shouldn't change between sets of simulations
+        self.main_deploy_altitude = main_deploy_altitude # meters
+        self.apogee = apogee # meters
+        self.velocity_off_rail_mag = velocity_off_rail_mag # m/s
 
-    vertical_drag_coeff_drogue = 1
-    vertical_drag_coeff_main = 1
-    transverse_drag_coeff_drogue = 1
-    transverse_drag_coeff_main = 1
+    # Drag coefficients
+        self.vertical_drag_coeff_drogue = vertical_drag_coeff_drogue
+        self.vertical_drag_coeff_main = vertical_drag_coeff_main
+        self.transverse_drag_coeff_drogue = transverse_drag_coeff_drogue
+        self.transverse_drag_coeff_main = transverse_drag_coeff_main
 
-    drogue_deploys_bool = True
-    main_deploys_bool = True
+    # Situational parameters (i.e. whether or not the parachutes deploy)
+        self.drogue_deploys_bool = drogue_deploys_bool
+        self.main_deploys_bool = main_deploys_bool
