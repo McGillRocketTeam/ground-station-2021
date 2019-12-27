0b71c0338836ad51ccf98e2ee02e5c718c2483b8
Alex Stedman <alexander.stedman@mail.mcgill.ca>
2019-12-27 11:10:57 -0800

    Fix angles not defined

--- Main.py
+++ Main.py
@@ -36,7 +36,7 @@ zenith_angle_perturbed = np.random.normal(launch_zenith_angle_in, sigma_zenith_a
 
 launch_azimuth_angle_in = 0     #from north to rocket
 sigma_azimuth_angle = 0
-azimuth_angle_perturbed = np.random.normal(launch_azimith_angle_in, sigma_azimith_angle, num_launches)
+azimuth_angle_perturbed = np.random.normal(launch_azimuth_angle_in, sigma_azimuth_angle, num_launches)
 
 drogue_deploy_altitude_in = 0
 wind_dataframe_in = 0
