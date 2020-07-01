a7bf6256d93e3735e86a1c126b5ac58b78405f6c
Jasper <jasper.yun@hotmail.com>
2020-06-30 22:39:44 -0400

    added verification of data written to telemetry CSV file

--- src/main/java/controller/datastorage/tests.java
+++ src/main/java/controller/datastorage/tests.java
@@ -159,18 +159,37 @@ public class tests {
 		double[] bad_telemetry_2 = {32.943008,1883089,1.26,0.000000,14,8.54,31.05,0.000000,-102,15,35,48};
 		double[] bad_telemetry_3 = {10, 12, 1.26};
 
-		Assert.assertTrue(DataStorage.saveDataCSV(TEST_FORMATTED_DATES, DataStorage.TELEMETRY, TEST_FILE_TIME, good_telemetry));
-		// should add something to check that the line written to the CSV file actually matches the stringed version of good_telemetry
+		boolean successful_saveData = DataStorage.saveDataCSV(TEST_FORMATTED_DATES, DataStorage.TELEMETRY, TEST_FILE_TIME, good_telemetry);
+		
+		// to check that the line was properly written to the CSV file
+		File file = new File(DataStorage.DATA_TYPE[DataStorage.TELEMETRY] + TEST_FILE_TIME + DataStorage.DATA_FILENAME[DataStorage.TELEMETRY]);
+		FileReader fr = null;
+		String line = "";
+		try {
+			fr = new FileReader(file);
+			BufferedReader br = new BufferedReader(fr);
+			br.readLine();
+			line = br.readLine();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		String expected_line = "1234-56-78 12-34-56-789,32.943008,-106.914925,1883089.0,1.26,0.0,10.0,8.54,31.05,0.0,-102.0,";
+		Assert.assertTrue(successful_saveData && expected_line.equals(line));		
 		System.out.println("test complete - telemetry CSV good");
 		
 		Assert.assertFalse(DataStorage.saveDataCSV(TEST_FORMATTED_DATES, DataStorage.STORAGE, TEST_FILE_TIME, good_telemetry));
 		System.out.println("test complete - telemetry CSV bad (STORAGE)");
+		
 		Assert.assertFalse(DataStorage.saveDataCSV(TEST_FORMATTED_DATES, DataStorage.SERIAL, TEST_FILE_TIME, good_telemetry));
 		System.out.println("test complete - telemetry CSV bad (SERIAL)");
+		
 		Assert.assertFalse(DataStorage.saveDataCSV(TEST_FORMATTED_DATES, DataStorage.TELEMETRY, TEST_FILE_TIME, bad_telemetry_1));
 		System.out.println("test complete - telemetry CSV bad (data 1)");
+		
 		Assert.assertFalse(DataStorage.saveDataCSV(TEST_FORMATTED_DATES, DataStorage.TELEMETRY, TEST_FILE_TIME, bad_telemetry_2));
 		System.out.println("test complete - telemetry CSV bad (data 2)");
+		
 		Assert.assertFalse(DataStorage.saveDataCSV(TEST_FORMATTED_DATES, DataStorage.TELEMETRY, TEST_FILE_TIME, bad_telemetry_3));
 		System.out.println("test complete - telemetry CSV bad (data 3)");
 		
