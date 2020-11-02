42ba7241b5130d373425d533eed3186a04dd1b37
jmrchow <jmrchow@gmail.com>
2020-11-02 17:16:54 -0500

    added fxml mainapp and css

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -2,24 +2,31 @@
 
 import java.io.IOException;
 
+import controller.gui.MainController;
 import javafx.application.Application;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 
 public class MainApp extends Application {
 
+
+	
     @Override
     public void start(Stage stage) throws Exception {
         Label l = new Label("McGill Rocket Team Ground Station");
         Parent root = FXMLLoader.load(getClass().getResource("fxml/MainApp.fxml"));
         Scene mainApp = new Scene(root, 1920,1080);
+
         stage.setTitle("McGill Rocket Team Ground Station");
         stage.setScene(mainApp);
         stage.show();
+
+
     }
 
     public static void main(String[] args) {
--- src/main/java/controller/gui/MainController.java
+++ src/main/java/controller/gui/MainController.java
@@ -1,6 +1,17 @@
 package controller.gui;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class MainController {
+	
+	@FXML
+	private Label peakAltitudeLabel;
+	
+	@FXML
+	public void setPeakAltitudeLabel(){
+		peakAltitudeLabel.setText("10");
+	}
 	public void initializeNumberDisplays() {
 		
 	}
