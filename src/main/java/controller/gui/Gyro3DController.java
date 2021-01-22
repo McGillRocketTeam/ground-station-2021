f80476f63c2eb3bc8d6084e6e4b26ee6c88d87f1
jmrchow <jmrchow@gmail.com>
2021-01-22 14:53:33 -0500

    vm backup commit

---
+++ src/main/java/controller/gui/Gyro3DController.java
@@ -0,0 +1,15 @@
+package controller.gui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class Gyro3DController {
+//	@FXML
+//	public Label testLabel;
+//	
+//	@FXML
+//	public void setTestLabel(String value){
+//		this.testLabel.setText(value);
+//	}
+
+}
