d5fe9a3f3e2d7f6ead6bc2c32a4f1271990ac28f
jeremychow <jmrchow@gmail.com>
2021-02-10 17:01:22 -0500

    moved code to controller from main

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -46,32 +46,18 @@ public class MainApp extends Application {
 	private final EnumMap<DataIndex, Integer> DataFormat = new EnumMap<DataIndex, Integer>(DataIndex.class);
 	private ScheduledExecutorService scheduledExecutorService;
 
-	final Group axisGroup = new Group();
-    final PerspectiveCamera camera = new PerspectiveCamera(true);
 
-    private static final double CAMERA_INITIAL_DISTANCE = -450;
-    private static final double CAMERA_INITIAL_X_ANGLE = 0;
-    private static final double CAMERA_INITIAL_Y_ANGLE = 0;
-    private static final double CAMERA_NEAR_CLIP = 0.1;
-    private static final double CAMERA_FAR_CLIP = 10000.0;
-    private static final double AXIS_LENGTH = 250.0;
     
     @Override
     public void start(Stage stage) throws Exception {
-    	Box box = new Box(100, 20, 50);
-    	SmartGroup group = new SmartGroup();
-    	group.getChildren().add(box);
-    	buildCamera();
-   //     buildAxes();
         Label l = new Label("McGill Rocket Team Ground Station");
-//        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("fxml/MainApp.fxml"));
-//        Parent root = fxmlLoader.load();
-//        Scene mainApp = new Scene(root, 1920,1080);
-//        MainAppController mainAppController = (MainAppController)fxmlLoader.getController();
-// 	      mainAppController.mainAppInitializeGraphs();
-//        mainAppController.mainAppInitializeMap();
-        Scene scene = new Scene(group, 1024, 768, true);
-        scene.setFill(Color.GREY);
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("fxml/MainApp.fxml"));
+        Parent root = fxmlLoader.load();
+        Scene mainApp = new Scene(root, 1920,1080);
+        MainAppController mainAppController = (MainAppController)fxmlLoader.getController();
+ 	    mainAppController.mainAppInitializeGraphs();
+        mainAppController.mainAppInitializeMap();
+
    
         stage.setTitle("McGill Rocket Team Ground Station");
         
@@ -114,11 +100,11 @@ public class MainApp extends Application {
 							System.out.println(data[3]);
 							Date now = new Date();
 							System.out.println(data[8]);
-							group.setRotateYAnimated(test);
-							group.setRotateXAnimated(test);
+//							group.setRotateYAnimated(test);
+//							group.setRotateXAnimated(test);
 							//cameraXform.rx.setAngle(data[8]);
-		//					mainAppControlle.mainAppAddGraphData(data, DataFormat);
-		//					mainAppController.startTimer(data,DataFormat);
+							mainAppController.mainAppAddGraphData(data, DataFormat);
+							mainAppController.startTimer(data,DataFormat);
 		
 					
 					});
@@ -132,9 +118,8 @@ public class MainApp extends Application {
 
 	
 //        stage.setScene(mainApp);
-        stage.setScene(scene);
+        stage.setScene(mainApp);
         stage.show();
-        scene.setCamera(camera);
 
 
     }
@@ -155,102 +140,7 @@ public class MainApp extends Application {
 		DataFormat.put(DataIndex.RSSI_INDEX, 9);
 	}
 	
-	private void buildCamera() {
-
-        camera.setNearClip(CAMERA_NEAR_CLIP);
-        camera.setFarClip(CAMERA_FAR_CLIP);
-        camera.setTranslateZ(CAMERA_INITIAL_DISTANCE);
-    }
-	private void buildAxes() {
-        final PhongMaterial redMaterial = new PhongMaterial();
-        redMaterial.setDiffuseColor(Color.DARKRED);
-        redMaterial.setSpecularColor(Color.RED);
- 
-        final PhongMaterial greenMaterial = new PhongMaterial();
-        greenMaterial.setDiffuseColor(Color.DARKGREEN);
-        greenMaterial.setSpecularColor(Color.GREEN);
- 
-        final PhongMaterial blueMaterial = new PhongMaterial();
-        blueMaterial.setDiffuseColor(Color.DARKBLUE);
-        blueMaterial.setSpecularColor(Color.BLUE);
- 
-        final Box xAxis = new Box(AXIS_LENGTH, 1, 1);
-        final Box yAxis = new Box(1, AXIS_LENGTH, 1);
-        final Box zAxis = new Box(1, 1, AXIS_LENGTH);
-        
-        xAxis.setMaterial(redMaterial);
-        yAxis.setMaterial(greenMaterial);
-        zAxis.setMaterial(blueMaterial);
- 
-        axisGroup.getChildren().addAll(xAxis, yAxis, zAxis);
-        axisGroup.setVisible(true);
-    //    root.getChildren().addAll(axisGroup);
-    }
-	
-	class SmartGroup extends Group {
-		Rotate r;
-		Transform t = new Rotate();
-		RotateTransition rXTransition = new RotateTransition(Duration.millis(100),this);
-		RotateTransition rYTransition = new RotateTransition(Duration.millis(100),this);
-		void rotateByX(double ang) {
-			r = new Rotate(ang, Rotate.X_AXIS);
-			t = t.createConcatenation(r);
-			this.getTransforms().clear();
-			this.getTransforms().addAll(t);
-		}
-
-		void rotateByY(double ang) {
-			r = new Rotate(ang, Rotate.Y_AXIS);
-			t = t.createConcatenation(r);
-			this.getTransforms().clear();
-			this.getTransforms().addAll(t);
-		}
-		
-		void setRotateX(double ang) {
-			t = new Rotate(ang, new Point3D(1,0,0));
-			this.getTransforms().add(t);
-
-		}
-		void setRotateY(double ang) {
-			t = new Rotate(ang, new Point3D(0,1,0));
-			this.getTransforms().add(t);
-		}
-		void setRotateXAnimated(double ang) {
-			rXTransition.setToAngle(ang);
-			rXTransition.setAxis(new Point3D(10,50,0));
-			rXTransition.play();
-		}
-		void setRotateYAnimated(double ang) {
-			rYTransition.setToAngle(ang);
-			rYTransition.setAxis(new Point3D(10,50,0));
-			rYTransition.play();
-		}
-		
-		/*
-		 * pitch (around its X axis), yaw (around its Y axis) and roll (around its Z axis),
-		 * alf is roll, bet is pitch and gam is yaw.
-		 * https://stackoverflow.com/questions/30145414/rotate-a-3d-object-on-3-axis-in-javafx-properly
-		 */
-		private void matrixRotateNode(Node n, double alf, double bet, double gam){
-		    double A11=Math.cos(alf)*Math.cos(gam);
-		    double A12=Math.cos(bet)*Math.sin(alf)+Math.cos(alf)*Math.sin(bet)*Math.sin(gam);
-		    double A13=Math.sin(alf)*Math.sin(bet)-Math.cos(alf)*Math.cos(bet)*Math.sin(gam);
-		    double A21=-Math.cos(gam)*Math.sin(alf);
-		    double A22=Math.cos(alf)*Math.cos(bet)-Math.sin(alf)*Math.sin(bet)*Math.sin(gam);
-		    double A23=Math.cos(alf)*Math.sin(bet)+Math.cos(bet)*Math.sin(alf)*Math.sin(gam);
-		    double A31=Math.sin(gam);
-		    double A32=-Math.cos(gam)*Math.sin(bet);
-		    double A33=Math.cos(bet)*Math.cos(gam);
-
-		    double d = Math.acos((A11+A22+A33-1d)/2d);
-		    if(d!=0d){
-		        double den=2d*Math.sin(d);
-		        Point3D p= new Point3D((A32-A23)/den,(A13-A31)/den,(A21-A12)/den);
-		        n.setRotationAxis(p);
-		        n.setRotate(Math.toDegrees(d));                    
-		    }
-		}
-		
-	}
 
 }
+
+
--- src/main/java/controller/gui/Gyro3DController.java
+++ src/main/java/controller/gui/Gyro3DController.java
@@ -1,15 +1,134 @@
 package controller.gui;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.animation.RotateTransition;
+import javafx.geometry.Point3D;
+import javafx.scene.Group;
+import javafx.scene.Node;
+import javafx.scene.PerspectiveCamera;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.PhongMaterial;
+import javafx.scene.shape.Box;
+import javafx.scene.transform.Rotate;
+import javafx.scene.transform.Transform;
+import javafx.util.Duration;
 
 public class Gyro3DController {
-//	@FXML
-//	public Label testLabel;
-//	
-//	@FXML
-//	public void setTestLabel(String value){
-//		this.testLabel.setText(value);
-//	}
+	final Group axisGroup = new Group();
+    final PerspectiveCamera camera = new PerspectiveCamera(true);
 
+    private static final double CAMERA_INITIAL_DISTANCE = -450;
+    private static final double CAMERA_INITIAL_X_ANGLE = 0;
+    private static final double CAMERA_INITIAL_Y_ANGLE = 0;
+    private static final double CAMERA_NEAR_CLIP = 0.1;
+    private static final double CAMERA_FAR_CLIP = 10000.0;
+    private static final double AXIS_LENGTH = 250.0;
+    
+    public void initializeGyro() {
+    	Box box = new Box(100, 20, 50);
+    	SmartGroup group = new SmartGroup();
+    	group.getChildren().add(box);
+    	buildCamera();
+        Scene scene = new Scene(group, 1024, 768, true);
+        scene.setFill(Color.GREY);
+    	
+        scene.setCamera(camera);
+    }
+    
+	private void buildCamera() {
+
+        camera.setNearClip(CAMERA_NEAR_CLIP);
+        camera.setFarClip(CAMERA_FAR_CLIP);
+        camera.setTranslateZ(CAMERA_INITIAL_DISTANCE);
+    }
+	private void buildAxes() {
+        final PhongMaterial redMaterial = new PhongMaterial();
+        redMaterial.setDiffuseColor(Color.DARKRED);
+        redMaterial.setSpecularColor(Color.RED);
+ 
+        final PhongMaterial greenMaterial = new PhongMaterial();
+        greenMaterial.setDiffuseColor(Color.DARKGREEN);
+        greenMaterial.setSpecularColor(Color.GREEN);
+ 
+        final PhongMaterial blueMaterial = new PhongMaterial();
+        blueMaterial.setDiffuseColor(Color.DARKBLUE);
+        blueMaterial.setSpecularColor(Color.BLUE);
+ 
+        final Box xAxis = new Box(AXIS_LENGTH, 1, 1);
+        final Box yAxis = new Box(1, AXIS_LENGTH, 1);
+        final Box zAxis = new Box(1, 1, AXIS_LENGTH);
+        
+        xAxis.setMaterial(redMaterial);
+        yAxis.setMaterial(greenMaterial);
+        zAxis.setMaterial(blueMaterial);
+ 
+        axisGroup.getChildren().addAll(xAxis, yAxis, zAxis);
+        axisGroup.setVisible(true);
+    //    root.getChildren().addAll(axisGroup);
+    }
+	
+	class SmartGroup extends Group {
+		Rotate r;
+		Transform t = new Rotate();
+		RotateTransition rXTransition = new RotateTransition(Duration.millis(100),this);
+		RotateTransition rYTransition = new RotateTransition(Duration.millis(100),this);
+		void rotateByX(double ang) {
+			r = new Rotate(ang, Rotate.X_AXIS);
+			t = t.createConcatenation(r);
+			this.getTransforms().clear();
+			this.getTransforms().addAll(t);
+		}
+
+		void rotateByY(double ang) {
+			r = new Rotate(ang, Rotate.Y_AXIS);
+			t = t.createConcatenation(r);
+			this.getTransforms().clear();
+			this.getTransforms().addAll(t);
+		}
+		
+		void setRotateX(double ang) {
+			t = new Rotate(ang, new Point3D(1,0,0));
+			this.getTransforms().add(t);
+
+		}
+		void setRotateY(double ang) {
+			t = new Rotate(ang, new Point3D(0,1,0));
+			this.getTransforms().add(t);
+		}
+		void setRotateXAnimated(double ang) {
+			rXTransition.setToAngle(ang);
+			rXTransition.setAxis(new Point3D(10,50,0));
+			rXTransition.play();
+		}
+		void setRotateYAnimated(double ang) {
+			rYTransition.setToAngle(ang);
+			rYTransition.setAxis(new Point3D(10,50,0));
+			rYTransition.play();
+		}
+		
+		/*
+		 * pitch (around its X axis), yaw (around its Y axis) and roll (around its Z axis),
+		 * alf is roll, bet is pitch and gam is yaw.
+		 * https://stackoverflow.com/questions/30145414/rotate-a-3d-object-on-3-axis-in-javafx-properly
+		 */
+		private void matrixRotateNode(Node n, double alf, double bet, double gam){
+		    double A11=Math.cos(alf)*Math.cos(gam);
+		    double A12=Math.cos(bet)*Math.sin(alf)+Math.cos(alf)*Math.sin(bet)*Math.sin(gam);
+		    double A13=Math.sin(alf)*Math.sin(bet)-Math.cos(alf)*Math.cos(bet)*Math.sin(gam);
+		    double A21=-Math.cos(gam)*Math.sin(alf);
+		    double A22=Math.cos(alf)*Math.cos(bet)-Math.sin(alf)*Math.sin(bet)*Math.sin(gam);
+		    double A23=Math.cos(alf)*Math.sin(bet)+Math.cos(bet)*Math.sin(alf)*Math.sin(gam);
+		    double A31=Math.sin(gam);
+		    double A32=-Math.cos(gam)*Math.sin(bet);
+		    double A33=Math.cos(bet)*Math.cos(gam);
+
+		    double d = Math.acos((A11+A22+A33-1d)/2d);
+		    if(d!=0d){
+		        double den=2d*Math.sin(d);
+		        Point3D p= new Point3D((A32-A23)/den,(A13-A31)/den,(A21-A12)/den);
+		        n.setRotationAxis(p);
+		        n.setRotate(Math.toDegrees(d));                    
+		    }
+		}
+	}
 }
