37a3fe33d20325c3dd99d389e89e6b7abff384f2
Danh4160 <63082166+Danh4160@users.noreply.github.com>
2021-08-09 14:43:51 -0400

    Graphs Blanche and simulated landing locations

--- Main.py
+++ Main.py
@@ -82,29 +82,70 @@ def main(num_launches):
         graph = plt.axes(projection='3d')
         graph.plot3D(timeData, latData, lonData, 'red')
         
-        graph.set_title("Simulation " + str(simNumber + 1))
+        graph.set_title("Simulation " + str(simNumber + 1) + " Lat vs Lon vs Time")
         graph.set_xlabel("Time")
         graph.set_ylabel("Latitude")
         graph.set_zlabel("Longitude")
 
-        #plt.savefig("simulation" + str(simNumber + 1) + "CoordinateGraph.png")
-        #plt.show()
+        # plt.savefig("simulation" + str(simNumber + 1) + "CoordinateGraph.png")
+        plt.show()
 
         # print(latData)
         # print(lonData)
         # print(zData)
 
-
     save_file = False  # If you want to save a csv file of the coordinates
-    #save_converted_positions = False  # Save the converted position in a text file
-
-    #if save_converted_positions:
-
     if save_file:
         np.savetxt("validation_compare_with_Blanche.csv", landing_location, delimiter=",")
         print("FIN CSV READY")
 
-    #print(positions[0])
+
+    # graph showing lat vs lon vs time from Blanche data set
+
+    with open('BlancheLat.txt', 'r') as f:
+        blancheLat = f.readlines()
+    blancheLat = [x.strip() for x in blancheLat]
+
+    with open('BlancheLon.txt', 'r') as f:
+        blancheLon = f.readlines()
+    blancheLon = [x.strip() for x in blancheLon]
+
+    with open('BlancheTime.txt', 'r') as f:
+        blancheTime = f.readlines()
+    blancheTime = [x.strip() for x in blancheTime]
+
+    for i in range(1272):
+        blancheLat[i] = float(blancheLat[i])
+        blancheLon[i] = float(blancheLon[i])
+        blancheTime[i] = float(blancheTime[i])
+
+    blancheLatData = np.array(blancheLat)
+    blancheLonData = np.array(blancheLon)
+    blancheTimeData = np.array(blancheTime)
+
+    blancheGraph = plt.axes(projection='3d')
+    blancheGraph.plot3D(blancheTimeData, blancheLatData, blancheLonData, 'blue')
+
+    blancheGraph.set_title("Blanche Lat vs Lon vs Time")
+    blancheGraph.set_xlabel("Time")
+    blancheGraph.set_ylabel("Latitude")
+    blancheGraph.set_zlabel("Longitude")
+
+    # plt.savefig("BlancheLatVsLonVsTimeGraph.png")
+    plt.show()
+
+    # graph showing simulation landing location and Blanche landing location
+    blancheLandingLocation = [32.945004, -106.89997]
+
+    landing_location = np.array(landing_location)
+    plt.plot(landing_location[:, 0], landing_location[:, 1], '0', color='black', label='Simulated Landing Location')
+    plt.plot(blancheLandingLocation[0], blancheLandingLocation[1], '0', color='black', label='Blanche Landing Location')
+    plt.legend()
+
+    # plt.savefig("SimulatedVsBlancheLandingLocation.png")
+    plt.show()
+
+
 
 
 
