46e663156a30153b20c5f9ecfac6a67b18c88d4a
Yu An Lu <yu-an.lu@outlook.com>
2022-01-16 14:24:03 -0500

    Setter for button
    and deleted some unused things

--- src/main/java/controller/gui/AltitudeGraphController.java
+++ src/main/java/controller/gui/AltitudeGraphController.java
@@ -1,49 +1,16 @@
 package controller.gui;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import javafx.animation.KeyFrame;
-import javafx.animation.Timeline;
-import javafx.application.Application;
-import javafx.application.Platform;
-import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.input.MouseButton;
-import javafx.scene.Group;
-import javafx.scene.Scene;
-import javafx.scene.chart.CategoryAxis;
+
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.FlowPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
-import javafx.stage.Stage;
-import javafx.stage.Window;
-import javafx.util.Duration;
-
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.EnumMap;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ThreadLocalRandom;
-import java.util.concurrent.TimeUnit;
 
 import javafx.fxml.FXML;
 
+import java.util.concurrent.ScheduledExecutorService;
 
 import org.gillius.jfxutils.JFXUtil;
 import org.gillius.jfxutils.chart.ChartPanManager;
@@ -51,89 +18,83 @@ import org.gillius.jfxutils.chart.FixedFormatTickFormatter;
 import org.gillius.jfxutils.chart.JFXChartUtil;
 import org.gillius.jfxutils.chart.StableTicksAxis;
 
-
 import controller.Parser;
 import javafx.fxml.FXMLLoader;
 
 import javafx.scene.input.ScrollEvent;
 import javafx.scene.Node;
-import javafx.event.EventHandler;
 
 public class AltitudeGraphController {
-	
+
 	public boolean isPlotFullHistory = false;
-	
+
 	final int window_size = 20;
 	ScheduledExecutorService scheduledExecutorService;
-	
+
 	@FXML
 	private LineChart<Number, Number> altitudeChart;
-	
+
 	XYChart.Series<Number, Number> altitudeData;
-	
+
 //	@FXML
 //	Button launchAltitudeGraph;
-	
+
 	public void initializeAltitudeGraph() {
 		initializeAltitudeChart();
 	}
-	
+
 	/**
-	*	Initializes the altitude chart on the main page
-	*/
+	 * Initializes the altitude chart on the main page
+	 */
 	private void initializeAltitudeChart() {
-		
+
 		altitudeData = new XYChart.Series<>();
 		altitudeData.setName("altitudeData");
 		altitudeChart.getData().add(altitudeData);
-		
+
 		NumberAxis yAxis = (NumberAxis) altitudeChart.getYAxis();
 		yAxis.setForceZeroInRange(false);
-		
+
 		NumberAxis xAxis = (NumberAxis) altitudeChart.getXAxis();
 		xAxis.setForceZeroInRange(false);
-		
+
 		JFXChartUtil.setupZooming(altitudeChart, new EventHandler<MouseEvent>() {
 			@Override
-			public void handle( MouseEvent mouseEvent ) {
-				if ( mouseEvent.getButton() != MouseButton.PRIMARY ||
-				     mouseEvent.isShortcutDown() )
+			public void handle(MouseEvent mouseEvent) {
+				if (mouseEvent.getButton() != MouseButton.PRIMARY || mouseEvent.isShortcutDown())
 					mouseEvent.consume();
 			}
-		} );
-		
+		});
+
 		JFXChartUtil.addDoublePrimaryClickAutoRangeHandler(altitudeChart);
 
 	}
-	
-	
+
 	public void addAltitudeGraphData(double[] data) {
-//		EventHandler <ActionEvent> event = new EventHandler <ActionEvent> () {
-//        	public void handle(ActionEvent e) {
 		addAltitudeData(data[DataIndex.TIME_INDEX.getOrder()], data[DataIndex.ALTITUDE_INDEX.getOrder()]);
-//        	}
-//		};
-//		launchAltitudeGraph.setOnAction(event);
-    }
-	
+
+	}
+
 	/**
 	 * Adds a data point to the altitude chart
+	 * 
 	 * @param x the time the data was measured in #TODO What units????
 	 * @param y the altitude that was measured in meters
 	 */
-	
 	private void addAltitudeData(Double x, Double y) {
-//		EventHandler <ActionEvent> event = new EventHandler <ActionEvent> () {
-//        	public void handle(ActionEvent e) {
-        		altitudeData.getData().add(new XYChart.Data<>(x, y));
-        if (!isPlotFullHistory) {
+		altitudeData.getData().add(new XYChart.Data<>(x, y));
+		if (!isPlotFullHistory) {
 
+			// Remove previous plot from graph
 			if (altitudeData.getData().size() > window_size)
 				altitudeData.getData().remove(0);
-	        	}
-//			};
-        }
-//		launchAltitudeGraph.setOnAction(event);
-        
-//	}
+		}
+	}
+
+	/**
+	 * Setter for button boolean
+	 */
+	public void setisPlotFullHistory() {
+		isPlotFullHistory = true;
+	}
 }
--- src/main/java/controller/gui/SceneController.java
+++ src/main/java/controller/gui/SceneController.java
@@ -21,22 +21,27 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 /**
- * SceneController is the controller class for the main GUI scene. It contains methods that control different part of the scene.
- * Once instantiated in MainApp, we can then control all functionalities in the GUI through this one class. 
- *  
+ * SceneController is the controller class for the main GUI scene. It contains
+ * methods that control different part of the scene. Once instantiated in
+ * MainApp, we can then control all functionalities in the GUI through this one
+ * class.
+ * 
  */
-
 
 public class SceneController {
 
-	//	=== Number Table ===
-			
-	//initialize variables
-	@FXML private AnchorPane numbertable;
-	@FXML private NumbertableController numbertableController;
-	
+	// === Number Table ===
+
+	// initialize variables
+	@FXML
+	private AnchorPane numbertable;
+	@FXML
+	private NumbertableController numbertableController;
+
 	/**
-	 * Start the asynchronous timer for updating the numbers display when running old data that is read in its entirety
+	 * Start the asynchronous timer for updating the numbers display when running
+	 * old data that is read in its entirety
+	 * 
 	 * @param data
 	 */
 	public void startTimer(double[] data) {
@@ -44,36 +49,44 @@ public class SceneController {
 		numbertableController.updateNumberDisplay(data);
 	}
 
-	//	=== Rocket Model ===
-	
-	//initialize variables
-	@FXML private AnchorPane rocketmodel;
-	@FXML private RocketModelController rocketmodelController;
+	// === Rocket Model ===
+
+	// initialize variables
+	@FXML
+	private AnchorPane rocketmodel;
+	@FXML
+	private RocketModelController rocketmodelController;
 
 	/**
 	 * Method to initialize the 3D Gyroscope view of the application
+	 * 
 	 * @throws Exception
 	 */
 	public void sceneInitializeGyro() throws Exception {
 		rocketmodel.getChildren().add(rocketmodelController.initializeGyro());
 	}
-	
+
 	/**
 	 * Method to add data to the 3D Gyroscope view of the application
+	 * 
 	 * @param data
 	 */
-	public void sceneAddGyroData(double[] data){
+	public void sceneAddGyroData(double[] data) {
 		rocketmodelController.addGyroData(data);
 	}
-	
-	//	=== Acceleration & Altitude Graphs ===
-	
-	//initialize variables
-	@FXML private AnchorPane accelerationgraphs;
-	@FXML private AccelerationGraphsController accelerationgraphsController;
-	@FXML private AltitudeGraphController altitudegraphController;
-	@FXML private ToggleButton launchAltitudeGraph;
-	
+
+	// === Acceleration & Altitude Graphs ===
+
+	// initialize variables
+	@FXML
+	private AnchorPane accelerationgraphs;
+	@FXML
+	private AccelerationGraphsController accelerationgraphsController;
+	@FXML
+	private AltitudeGraphController altitudegraphController;
+	@FXML
+	private ToggleButton launchAltitudeGraph;
+
 	/**
 	 * Initialize the graphs
 	 */
@@ -81,57 +94,60 @@ public class SceneController {
 		accelerationgraphsController.initializeGraphs();
 		altitudegraphController.initializeAltitudeGraph();
 	}
-	
+
 	/**
 	 * Method to add data to the graphs
+	 * 
 	 * @param data array of telemetry data
 	 */
 	public void sceneAddGraphData(double[] data) {
 		accelerationgraphsController.addGraphData(data);
 		altitudegraphController.addAltitudeGraphData(data); // add points
-		
+
 		EventHandler<ActionEvent> event = new EventHandler<ActionEvent>() {
 			public void handle(ActionEvent e) {
-				altitudegraphController.isPlotFullHistory = true; // toggle
-
-				//altitudegraphController.addAltitudeGraphData(data);
+				altitudegraphController.setisPlotFullHistory(); // toggle button
 			}
 		};
+
 		launchAltitudeGraph.setOnAction(event);
 	}
-	
-	//	=== Launch Page ===
-	
-	@FXML Button launchButton;
-	
+
+	// === Launch Page ===
+
+	@FXML
+	Button launchButton;
+
 	/**
-	 * Creates a new Scene, the Launch Page. Launch Page becomes visible once the "Launch Page" button is clicked. 
+	 * Creates a new Scene, the Launch Page. Launch Page becomes visible once the
+	 * "Launch Page" button is clicked.
+	 * 
 	 * @throws Exception
 	 */
 	public void initializeScene() throws Exception {
-		
-		
-		EventHandler <ActionEvent> event = new EventHandler <ActionEvent> () {
-        	public void handle(ActionEvent e) {
-        		
+
+		EventHandler<ActionEvent> event = new EventHandler<ActionEvent>() {
+			public void handle(ActionEvent e) {
+
 //        		e.consume();
 
-        		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml_21_22/LaunchPage.fxml"));
-        		//	FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("fxml/MainApp.fxml"));
+				FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml_21_22/LaunchPage.fxml"));
+				// FXMLLoader fxmlLoader = new
+				// FXMLLoader(getClass().getResource("fxml/MainApp.fxml"));
 
 				try {
 					Parent launchRoot = fxmlLoader.load();
-					Scene launchScene = new Scene(launchRoot, 800,600);
+					Scene launchScene = new Scene(launchRoot, 800, 600);
 
-	                Stage newWindow = new Stage();
-	                newWindow.setTitle("Launch");
-	                newWindow.setScene(launchScene);
-	                newWindow.show();
+					Stage newWindow = new Stage();
+					newWindow.setTitle("Launch");
+					newWindow.setScene(launchScene);
+					newWindow.show();
 				} catch (IOException e1) {
 					// TODO Auto-generated catch block
 					System.out.println("oh no");
 				}
-        		
+
 //        		StackPane secondaryLayout = new StackPane();
 //                
 //                Scene secondScene = new Scene(secondaryLayout, 500,500);
@@ -139,9 +155,9 @@ public class SceneController {
 //                newWindow.setTitle("Launch");
 //                newWindow.setScene(secondScene);
 //                newWindow.show();
-            } 
-        }; 
-        
-        launchButton.setOnAction(event);
+			}
+		};
+
+		launchButton.setOnAction(event);
 	}
 }
